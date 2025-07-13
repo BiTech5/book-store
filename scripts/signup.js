3fdb8caffe418532acf0f59a416aec4ccf1c3327
Bikram <bikikc7@gmail.com>
2025-07-13 19:08:25 +0545

    making singup

---
+++ scripts/signup.js
@@ -0,0 +1,15 @@
+const signupForm = document.getElementById('signupForm');
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('mail').value;
+    const password = document.getElementById('password').value;
+
+    const user = { name, email, password };
+
+    localStorage.setItem('user', JSON.stringify(user));
+    alert("Account created successfully! Please login.");
+    window.location.href = 'login.html';
+});
